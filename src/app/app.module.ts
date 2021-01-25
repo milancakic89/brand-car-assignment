3bf935a3fabcb22a2e3d502a5bbdd3b5f24f1d98
Milan Cakic <58387889+milancakic89@users.noreply.github.com>
2021-01-25 17:09:40 +0100

    Added car details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { Service } from './app.service';
 
 
 import { AppComponent } from './app.component';
 import { CarTableComponent } from './car-table/car-table.component';
+import { CarDetailsComponent } from './car-details/car-details.component';
+
+const routes: Routes = [
+  { path: '', component: CarTableComponent },
+  { path: '/brand/:id', component: CarDetailsComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarTableComponent
+    CarTableComponent,
+    CarDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [Service],
   bootstrap: [AppComponent]
